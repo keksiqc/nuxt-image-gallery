4f373b540fe8ee0bad42f7e7322a32d2a953134c
Keksi <marlonhotz07@gmail.com>
2024-05-12 23:18:50 +0200

    improve

--- server/api/images/upload.post.ts
+++ server/api/images/upload.post.ts
@@ -1,10 +1,23 @@
-export default eventHandler(async (event) => {
+import { defineEventHandler, readFormData } from 'h3'
+import { ensureBlob, hubBlob } from '~/utils'
+import { requireUserSession } from '~/middleware'
+
+export default defineEventHandler(async (event) => {
+  // Check if user is authenticated
   await requireUserSession(event)
+
+  // Read form data from the request
   const form = await readFormData(event)
+
+  // Get the image file from the form data
   const image = form.get('image') as File
 
-  // @ts-expect-error: jpg format
-  ensureBlob(image, { maxSize: '8MB', types: ['image/jpeg', 'image/png', 'image/gif', 'image/heic', 'image/webp', 'image/jpg'] })
+  // Ensure the image is a valid format and size
+  await ensureBlob(image, {
+    maxSize: '8MB',
+    types: ['image/jpeg', 'image/png', 'image/gif', 'image/heic', 'image/webp', 'image/jpg']
+  })
 
+  // Upload the image to the hub
   return hubBlob().put(image.name, image)
 })
