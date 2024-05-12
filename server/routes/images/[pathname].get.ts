9ba0a61787311fc101a04921a8c1f0a7d2240cdc
Keksi <marlonhotz07@gmail.com>
2024-05-12 23:18:54 +0200

    improve

--- server/routes/images/[pathname].get.ts
+++ server/routes/images/[pathname].get.ts
@@ -1,9 +1,13 @@
 import { z } from 'zod'
+import { eventHandler, getValidatedRouterParams } from 'h3'
+import { serveHubBlob } from '../utils'
+
+const routeParamsSchema = z.object({
+  pathname: z.string().min(1)
+})
 
 export default eventHandler(async (event) => {
-  const { pathname } = await getValidatedRouterParams(event, z.object({
-    pathname: z.string().min(1)
-  }).parse)
-
-  return hubBlob().serve(event, pathname)
+  const { pathname } = await getValidatedRouterParams(event, routeParamsSchema)
+  return serveHubBlob(event, pathname)
 })
+
