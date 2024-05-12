edd8533b4d9542500105879fbd2dd1bfc08f017e
Keksi <marlonhotz07@gmail.com>
2024-05-12 23:18:56 +0200

    improve

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,43 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+// .eslintrc.js
 
-export default withNuxt({
-  // Your custom configs here
-})
+import withNuxt from './.nuxt/eslint.config.mjs'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default {
+  ...withNuxt(),
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:nuxt/recommended',
+    'prettier',
+    'prettier/vue',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'vue/multi-word-component-names': 'off',
+  },
+  overrides: [
+    {
+      files: ['components/**/*.vue'],
+      rules: {
+        'vue/component-definition-name-casing': ['error', 'PascalCase'],
+      },
+    },
+  ],
+  settings: {
+    'vue/component-prefer-generic-property-access': true,
+  },
+}
