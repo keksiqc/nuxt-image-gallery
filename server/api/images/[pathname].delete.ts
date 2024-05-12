47fdc3f02b59fed5db9731d094408dbd9e08a486
Keksi <marlonhotz07@gmail.com>
2024-05-12 23:18:55 +0200

    improve

--- server/api/images/[pathname].delete.ts
+++ server/api/images/[pathname].delete.ts
@@ -1,6 +1,20 @@
-export default eventHandler(async (event) => {
-  await requireUserSession(event)
-  const { pathname } = event.context.params || {}
+import { deleteObject as deleteHubBlob } from 'hubBlob'
+import { handleError } from './errorHandler'
+import { getUserSession } from './sessionHandler'
 
-  return hubBlob().delete(pathname)
-})
+export default async function deleteBlob(event) {
+  try {
+    const userSession = await getUserSession(event)
+    const { pathname } = event.context.params || {}
+
+    if (!pathname) {
+      throw new Error('Missing pathname parameter')
+    }
+
+    await deleteHubBlob(pathname, { userSession })
+
+    return { status: 200 }
+  } catch (error) {
+    return handleError(error)
+  }
+}
