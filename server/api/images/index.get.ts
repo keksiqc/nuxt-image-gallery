d715d043795db39f8641180c3515ffe533b83bef
Keksi <marlonhotz07@gmail.com>
2024-05-12 23:18:48 +0200

    improve

--- server/api/images/index.get.ts
+++ server/api/images/index.get.ts
@@ -1,3 +1,12 @@
-export default eventHandler(async () => {
-  return hubBlob().list()
+import { eventHandler } from 'nuxt-event-handlers'
+import { hubBlob } from 'your-blob-module'
+
+export default eventHandler(async (event) => {
+  try {
+    const list = await hubBlob().list()
+    return event.respondWith(new Response(JSON.stringify(list), { status: 200 }))
+  } catch (error) {
+    return event.respondWith(new Response(JSON.stringify({ error: error.message }), { status: 500 }))
+  }
 })
+
